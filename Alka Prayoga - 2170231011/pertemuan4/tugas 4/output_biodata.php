0284910958ee8c9a915d73a63e1c8caf0e6d6ef8
AlYogzzzzss <95873625+AlYogzzzzss@users.noreply.github.com>
2022-12-16 22:42:47 +0700

    Add files via upload
    praktikum Pemweb

---
+++ Alka Prayoga - 2170231011/pertemuan4/tugas 4/output_biodata.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html> <html>
+<head>
+<title>Biodata</title> </head>
+<body>
+<h1><center>BIODATA DIRI</center></h1>
+</body>
+</html>
+<?php $Nama=$_POST['Nama']; $NIM=$_POST['NIM']; $JK=$_POST['JK']; $Tempat=$_POST['Tempat']; $tanggal=$_POST['tanggal']; $alamat=$_POST['alamat']; $jurusan=$_POST['jurusan']; $hobby=$_POST['hobby'];
+echo "Nama : Alka Prayoga $Nama<br>";
+
+echo "NIM : 2170231011 $NIM<br>";
+echo "Jenis Kelamin : Laki-Laki $JK<br>";
+echo "Tempat dan Tanggal Lahir : Bogor_15_06_2002 $Tempat, $tanggal<br>"; echo "Alamat : Sukaati $alamat<br>";
+echo "Jurusan : Teknik_Informatika$jurusan<br>";
+echo "Hobby : Banyak $hobby<br>";
+?>
